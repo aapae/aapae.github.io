3126b01c559ea0407aa6598ca770983ea80f1d0f
Dave Kinkead <dave@kinkead.com.au>
2026-05-26 12:33:28 +0200

    Allow display banner to be toggle-able

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,20 +1,39 @@
 <script>
+  export let displayBanner = false;
   let menuOpen = false;
 </script>
 
 <div class="topbar">
-  <p class="notice">
-    <a href="/events/cfp-ethics-of-ai">
-      Call for papers: The Ethics of Artificial Intelligence and the Professions<br />
-      Special edition of Research in Ethical Issues in Organizations, the official journal of the AAPAE.
-    </a>
-  </p>
+  {#if displayBanner}
+    <p class="notice">
+      <a href="/events/cfp-ethics-of-ai">
+        Call for papers: The Ethics of Artificial Intelligence and the
+        Professions<br />
+        Special edition of Research in Ethical Issues in Organizations, the official
+        journal of the AAPAE.
+      </a>
+    </p>
+  {/if}
 
   <nav class="site navigation">
     <div class="trigger">
-      <button class="trigger-btn" on:click={() => (menuOpen = !menuOpen)} aria-label="Toggle menu">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list menu" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+      <button
+        class="trigger-btn"
+        on:click={() => (menuOpen = !menuOpen)}
+        aria-label="Toggle menu"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-list menu"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"
+          />
         </svg>
         AAPAE
       </button>
