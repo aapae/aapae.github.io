63a9c6c99eb03d246f7e5b9c726a9c1c172a179a
Dave Kinkead <dave@kinkead.com.au>
2026-05-27 17:26:04 +0200

    Update footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,36 @@
 <footer>
-  <div class="wrapper">
+  <nav>
+    <div class="logo">
+      <a href="/"><img src="/logos/color.png" /></a>
+    </div>
+    <div class="nav-group">
+      <h5>Association</h5>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+      <a href="/people">People</a>
+      <a href="/membership">Membership</a>
+      <a href="/notices">Notices</a>
+    </div>
+    <div class="nav-group">
+      <h5>Research</h5>
+      <a href="/publications">Publications</a>
+      <a href="/newsletters">Newsletters</a>
+      <a href="/book-reviews">Book Reviews</a>
+      <a href="/australian-ethics">Australian Ethics</a>
+      <a
+        href="/https://www.emerald.com/collection/253/Research-in-Ethical-Issues-in-Organizations"
+        >REIO</a
+      >
+    </div>
+    <div class="nav-group">
+      <h5>Resources</h5>
+      <a href="/blog">Blog</a>
+      <a href="/events">Events</a>
+      <a href="/reports">Reports</a>
+      <a href="/search">Search</a>
+    </div>
+  </nav>
+  <div class="final">
     <span>&copy; 2002–{new Date().getFullYear()} AAPAE</span>
     <span>ABN: 91 541 307 476</span>
     <span><a href="mailto:info@aapae.org.au">info@aapae.org.au</a></span>
