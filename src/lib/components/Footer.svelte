467a68493eb9e59309dff9e979a87727caaf2fa6
Dave Kinkead <dave@kinkead.com.au>
2026-05-28 07:41:55 +0200

    Remove unneeded nav from the footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -9,22 +9,19 @@
       <a href="/contact">Contact</a>
       <a href="/people">People</a>
       <a href="/membership">Membership</a>
-      <a href="/notices">Notices</a>
     </div>
     <div class="nav-group">
       <h5>Research</h5>
       <a href="/publications">Publications</a>
       <a href="/newsletters">Newsletters</a>
-      <a href="/book-reviews">Book Reviews</a>
       <a href="/australian-ethics">Australian Ethics</a>
       <a
-        href="/https://www.emerald.com/collection/253/Research-in-Ethical-Issues-in-Organizations"
+        href="https://www.emerald.com/collection/253/Research-in-Ethical-Issues-in-Organizations"
         >REIO</a
       >
     </div>
     <div class="nav-group">
       <h5>Resources</h5>
-      <a href="/blog">Blog</a>
       <a href="/events">Events</a>
       <a href="/reports">Reports</a>
       <a href="/search">Search</a>
